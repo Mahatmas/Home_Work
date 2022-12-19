bfc6fa6ea39284c0c1dc2107c38e88df4b246c90
Ruslan <kolibskiy@yandex.ru>
2022-12-19 14:42:07 +0300

    Все задачи урока решены

--- Program.cs
+++ Program.cs
@@ -12,3 +12,56 @@ else
 {
     Console.WriteLine(b);
 }
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите третее число: ");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+Console.WriteLine(max);
+
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Введите число: ");
+
+int a = int.Parse(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+   Console.WriteLine("Да");
+}
+else
+{
+   Console.WriteLine("Нет");
+}
+
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+    
+int num = 1;
+
+while (num <= N)
+{
+    if (num % 2 == 0)
+    Console.Write($"{num} ");
+    num++;
+}
